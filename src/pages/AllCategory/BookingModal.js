2d8dfb8ffe6fa6dcb17870d2de11c26832144a84
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2023-01-14 23:17:26 +0600

    order added

--- src/pages/AllCategory/BookingModal.js
+++ src/pages/AllCategory/BookingModal.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "react-bootstrap/Modal";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
 
 const BookingModal = ({ show, handleClose, modalData }) => {
+  const { user } = useContext(AuthContext);
+
   const {
     productName,
     resPrice,
@@ -10,7 +14,49 @@ const BookingModal = ({ show, handleClose, modalData }) => {
     location,
     phNumber,
     proImg,
+    _id,
   } = modalData;
+
+  const navigate = useNavigate();
+  const handleAddOrder = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const userName = form.userName.value;
+    const userEmail = form.userEmail.value;
+    const sellPrice = form.sellPrice.value;
+    const userLocation = form.userLocation.value;
+    const userNumber = form.userNumber.value;
+    console.log(userName, userEmail, sellPrice, userLocation, userNumber);
+    const order = {
+      userName,
+      userEmail,
+      sellPrice,
+      userLocation,
+      userNumber,
+      sellerName,
+      sellerEmail,
+      location,
+      phNumber,
+      proImg,
+      productId: _id,
+    };
+
+    fetch("http://localhost:5000/addOrders", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        navigate("/myOrders");
+        form.reset();
+      });
+  };
+
   return (
     <Modal show={show} centered onHide={handleClose}>
       <Modal.Header closeButton>
@@ -19,53 +65,91 @@ const BookingModal = ({ show, handleClose, modalData }) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div className="d-flex">
-          <div className="mb-3 w-50 me-2">
+        <form onSubmit={handleAddOrder}>
+          <div className="d-flex">
+            <div className="mb-3 w-50 me-2">
+              <label
+                htmlFor="exampleInputImg"
+                className="form-label fw-semibold"
+              >
+                User Name
+              </label>
+              <input
+                disabled
+                name="userName"
+                defaultValue={user?.displayName}
+                type="text"
+                className="form-control"
+                id="exampleInp"
+              />
+            </div>
+            <div className="mb-3 w-50 ms-1">
+              <label
+                htmlFor="exampleInputImg"
+                className="form-label fw-semibold"
+              >
+                User Email
+              </label>
+              <input
+                defaultValue={user?.email}
+                disabled
+                name="userEmail"
+                type="text"
+                className="form-control"
+                id="exampleInput"
+              />
+            </div>{" "}
+          </div>
+          <div className="d-flex">
+            <div className="mb-3 w-50 me-2">
+              <label
+                htmlFor="exampleInputImg"
+                className="form-label fw-semibold"
+              >
+                Selling Price
+              </label>
+              <input
+                defaultValue={resPrice}
+                disabled
+                name="sellPrice"
+                type="text"
+                className="form-control"
+                id="exampleInput"
+              />
+            </div>
+            <div className="mb-3 w-50 ms-1">
+              <label
+                htmlFor="exampleInputImg"
+                className="form-label fw-semibold"
+              >
+                Location
+              </label>
+              <input
+                name="userLocation"
+                type="text"
+                className="form-control"
+                id="exampleInput"
+              />
+            </div>
+          </div>{" "}
+          <div className="mb-3 w-full">
             <label htmlFor="exampleInputImg" className="form-label fw-semibold">
-              Seller Name
+              Your Phone Number
             </label>
             <input
-              disabled
-              name="sellerName"
-              defaultValue={sellerName}
+              name="userNumber"
               type="text"
               className="form-control"
-              id="exampleInp"
+              id="exampleInput"
             />
           </div>
-          <div className="mb-3 w-50 ms-1">
-            <label htmlFor="exampleInputImg" className="form-label fw-semibold">
-              Seller Email
-            </label>
-            <input
-              defaultValue={sellerEmail}
-              disabled
-              name="sellerEmail"
-              type="text"
-              className="form-control"
-              id="exampleInput"
-            />
-          </div>{" "}
-        </div>
-        <div className="d-flex">
-          <div className="mb-3 w-50 ms-1">
-            <label htmlFor="exampleInputImg" className="form-label fw-semibold">
-              Selling Price
-            </label>
-            <input
-              defaultValue={resPrice}
-              disabled
-              name="sellerEmail"
-              type="text"
-              className="form-control"
-              id="exampleInput"
-            />
-          </div>{" "}
-        </div>
+          <div className="px-5 py-2">
+            <button type="submit" className="btn btn-primary w-100">
+              Add To Order
+            </button>
+          </div>
+        </form>
       </Modal.Body>
-      <div className="px-5 py-2">
-        <button className="btn btn-primary w-100">Add To Order</button>
-      </div>
     </Modal>
   );
 };
